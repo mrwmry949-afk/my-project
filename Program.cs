c490756ce217257909c0b25497d6befc7b40beee
AМR Marei <mrwmry949@gmail.com>
2026-08-17 15:56:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace C__Fundamental.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
