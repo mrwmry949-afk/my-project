e2031ff6321ea1bb6bdfea0e0f96b8b82d89b7f9
AМR Marei <mrwmry949@gmail.com>
2026-08-17 16:16:48 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,31 +5,25 @@
         static void Main(string[] args)
 
         {
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name);
+            string[] input = Console.ReadLine().Split();
 
+            int a = int.Parse(input[0]);
+            long b = long.Parse(input[1]);
+            char c = char.Parse(input[2]);
+            float d = float.Parse(input[3]);
+            double e = double.Parse(input[4]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
         }
     }
 }
+
+
+
 
 
 
