de2c6882a8c7fc7360c1726f17598cc182e3257b
AМR Marei <mrwmry949@gmail.com>
2026-08-17 16:49:23 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -5,19 +5,13 @@
         static void Main(string[] args)
 
         {
-            string[] input = Console.ReadLine().Split();
-
-            int a = int.Parse(input[0]);
-            long b = long.Parse(input[1]);
-            char c = char.Parse(input[2]);
-            float d = float.Parse(input[3]);
-            double e = double.Parse(input[4]);
-
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
+            string[] data = Console.ReadLine().Split();
+            long x, y;
+            x = int.Parse(data[0]);
+            y = int.Parse(data[1]);
+            Console.WriteLine($"{x} + {y} = {x + y}");
+            Console.WriteLine($"{x} * {y} = {x * y}");
+            Console.WriteLine($"{x} - {y} = {x - y}");
         }
     }
 }
